82339fa0eeef7c6762f7473aca288781577b9949
Michael Bagnall <mrbagnall@icloud.com>
2019-09-22 09:40:30 -0400

    Finish up the issues with making cron what we want

--- bash/run-httpd.sh
+++ bash/run-httpd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Replace our cron interval with what has been suggested
-envsubst < /etc/cron.d/minio > /etc/httpd/conf/minio-cron
+envsubst < /etc/cron.d/minio > /etc/cron.d/minio-cron
 
 # Set up the cron in our set of crontabs
-crontab /etc/cron.d/minio
+crontab /etc/cron.d/minio-cron
+rm /etc/cron.d/minio
 
 # Stop crond
 pkill crond
